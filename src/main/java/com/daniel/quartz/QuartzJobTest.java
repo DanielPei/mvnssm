680509c3abe2788d7e6ae9a9d25540fd9f338e6a
DanielPei <peixq1222@icloud.com>
2017-08-16 15:53:01 +0800

    Add quartz test demo

---
+++ src/main/java/com/daniel/quartz/QuartzJobTest.java
@@ -0,0 +1,12 @@
+package com.daniel.quartz;
+
+import java.util.Date;
+
+public class QuartzJobTest{
+	
+	public void displayTime() {
+		Date dt = new Date();
+		System.out.println(dt.toString());
+	}
+
+}
